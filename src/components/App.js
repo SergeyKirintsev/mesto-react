4cfe389c16ba9078fcce8c1e54d5b0814e4013d4
SergeyKirintsev <kserg80@gmail.com>
2021-05-02 09:29:58 +0300

    создать компонент Main

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,12 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Main from "./Main";
 
 function App() {
   return (
     <div className="page">
       <Header />
-
-      <main className="content">
-        <section className="profile">
-          <div className="profile__avatar-wrapper">
-            <img className="profile__avatar" src="#" alt="аватар"/>
-            <div className="profile__avatar-overlay"></div>
-          </div>
-          <div className="profile__info">
-            <div className="profile__row">
-              <h1 className="profile__name block">Человек с длинным именем</h1>
-              <button
-                type="button"
-                aria-label="Редактировать профиль"
-                className="profile__edit-btn btn-hover"
-              ></button>
-            </div>
-            <p className="profile__profession block">
-              Исследователь океана и всего что можно
-            </p>
-          </div>
-
-          <button
-            type="button"
-            aria-label="Добавить карточку"
-            className="profile__add-btn btn-hover"
-          ></button>
-        </section>
-
-        <section className="elements">
-          <ul className="elements__list">
-          </ul>
-        </section>
-      </main>
-
+      <Main />
       <Footer />
     </div>
   );
---
+++ src/components/Main.js
@@ -0,0 +1,38 @@
+function Main(props) {
+  return (
+    <main className="content">
+      <section className="profile">
+        <div className="profile__avatar-wrapper">
+          <img className="profile__avatar" src="#" alt="аватар"/>
+          <div className="profile__avatar-overlay"></div>
+        </div>
+        <div className="profile__info">
+          <div className="profile__row">
+            <h1 className="profile__name block">Человек с длинным именем</h1>
+            <button
+              type="button"
+              aria-label="Редактировать профиль"
+              className="profile__edit-btn btn-hover"
+            ></button>
+          </div>
+          <p className="profile__profession block">
+            Исследователь океана и всего что можно
+          </p>
+        </div>
+
+        <button
+          type="button"
+          aria-label="Добавить карточку"
+          className="profile__add-btn btn-hover"
+        ></button>
+      </section>
+
+      <section className="elements">
+        <ul className="elements__list">
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
