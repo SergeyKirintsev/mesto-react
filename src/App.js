62f08f5d3018c3f6ffdbecabb9438731d7e9a00c
SergeyKirintsev <kserg80@gmail.com>
2021-05-02 09:04:02 +0300

    портировать стили и разметку

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="page">
+      <header className="header">
+        <img className="header__logo" src={logo} alt="логотип"/>
       </header>
+
+      <main className="content">
+        <section className="profile">
+          <div className="profile__avatar-wrapper">
+            <img className="profile__avatar" src="#" alt="аватар"/>
+            <div className="profile__avatar-overlay"></div>
+          </div>
+          <div className="profile__info">
+            <div className="profile__row">
+              <h1 className="profile__name block">Человек с длинным именем</h1>
+              <button
+                type="button"
+                aria-label="Редактировать профиль"
+                className="profile__edit-btn btn-hover"
+              ></button>
+            </div>
+            <p className="profile__profession block">
+              Исследователь океана и всего что можно
+            </p>
+          </div>
+
+          <button
+            type="button"
+            aria-label="Добавить карточку"
+            className="profile__add-btn btn-hover"
+          ></button>
+        </section>
+
+        <section className="elements">
+          <ul className="elements__list">
+          </ul>
+        </section>
+      </main>
+
+      <footer className="footer">
+        <p className="footer__copyright">&copy; 2021. Киринцев Сергей</p>
+      </footer>
     </div>
   );
 }
